f37940bd84bbea23de8908fdbf3d4374cda3a3e0
kadowakiyuya <yuya.kadowaki@avantcorp.com>
2025-07-11 14:07:09 +0900

    コミット

--- src/main/java/com/example/simplezakka/repository/OrderDetailRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.simplezakka.repository;
-
-import com.example.simplezakka.entity.OrderDetail;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface OrderDetailRepository extends JpaRepository<OrderDetail, Integer> {
-}
