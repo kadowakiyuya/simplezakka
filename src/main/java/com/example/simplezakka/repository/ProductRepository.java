ee3b668fab7902c38c6448f6d527ea6c0ec25725
yuyanakagawa <yuya.nakagawa@avantcorp.com>
2025-07-25 15:39:43 +0900

    3-10~12完成

--- src/main/java/com/example/simplezakka/repository/ProductRepository.java
+++ src/main/java/com/example/simplezakka/repository/ProductRepository.java
@@ -31,6 +31,8 @@ public interface ProductRepository extends JpaRepository<Product, Integer> {
     @Modifying
     @Query("UPDATE Product p SET p.stock = p.stock - ?2 WHERE p.productId = ?1 AND p.stock >= ?2")
     int decreaseStock(Integer productId, Integer quantity);
+
+    //void setStockQuantity(int i);
    
 
 }
